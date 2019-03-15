927696557a69a71517e5665f6089e691aeed40d3
Giacomo S <giacomo.strangolino@elettra.eu>
2019-03-15 14:57:05 +0100

    added plugin interface

---
+++ src/cubotplugininterface.cpp
@@ -0,0 +1,6 @@
+#include "cubotplugininterface.h"
+
+CuBotPluginInterface::CuBotPluginInterface()
+{
+
+}
---
+++ src/cubotplugininterface.h
@@ -0,0 +1,11 @@
+#ifndef CUBOTPLUGININTERFACE_H
+#define CUBOTPLUGININTERFACE_H
+
+
+class CuBotPluginInterface
+{
+public:
+    CuBotPluginInterface();
+};
+
+#endif // CUBOTPLUGININTERFACE_H
