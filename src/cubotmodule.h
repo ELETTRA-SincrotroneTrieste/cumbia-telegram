8068553a7bd22273402efaaa1df1d4a45fcb3fb9
Giacomo S <giacomo.strangolino@elettra.eu>
2019-03-19 17:10:26 +0100

    migration to modules/plugins

---
+++ src/cubotmodule.h
@@ -0,0 +1,11 @@
+#ifndef CUBOTMODULE_H
+#define CUBOTMODULE_H
+
+
+class CuBotModule
+{
+public:
+    CuBotModule();
+};
+
+#endif // CUBOTMODULE_H
--- src/cubotplugininterface.cpp
+++
@@ -1,6 +0,0 @@
-#include "cubotplugininterface.h"
-
-CuBotPluginInterface::CuBotPluginInterface()
-{
-
-}
